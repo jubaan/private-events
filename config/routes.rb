71747dcb1ead791de8b28e143f69b934d0d3117a
jubaan <julio.ab@outlook.com>
2020-06-09 17:48:11 -0500

    Add event model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :user
   devise_for :users
+  resources :users
+  resources :events
+
   root "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
