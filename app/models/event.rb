9274444cac0ad6722dcd3ccae476a4d6b6d9da8a
jubaan <julio.ab@outlook.com>
2020-06-11 15:27:03 -0500

    every user can see their created events and the events created by others

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
   belongs_to :host, foreign_key: 'host_id', class_name: 'User'
   has_many :appointments, foreign_key: 'event_id', dependent: :destroy
   has_many :attendees, through: :appointments
+
+  scope :upcoming, -> { where('date >=?', Date.today) }
+  scope :past, -> { where('date <?', Date.today) }
 end
