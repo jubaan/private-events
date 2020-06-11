332dc6a5f96ed48d9f090e22fce99eda85b668d6
jubaan <julio.ab@outlook.com>
2020-06-11 11:58:13 -0500

    Attendee can create appointments to events, Individual events show host and attendees

--- db/migrate/20200610162148_create_appointments.rb
+++ db/migrate/20200610162148_create_appointments.rb
@@ -3,7 +3,7 @@
 class CreateAppointments < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
-      t.references :attendee, null: false, foreign_key: { to_table: :users }
+      t.references :attendee, null: false, foreign_key: { to_table: :users }, unique: true
       t.references :event, null: false, foreign_key: { to_table: :events }
       t.timestamps
     end
