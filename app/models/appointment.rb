df25bf86b50135ac78e8b0fb0b2ef64971748f90
jubaan <julio.ab@outlook.com>
2020-06-12 16:52:17 -0500

    Created method to confirm user invites

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,4 +4,12 @@ class Appointment < ApplicationRecord
   belongs_to :attendee, foreign_key: 'attendee_id', class_name: 'User'
   belongs_to :event, foreign_key: 'event_id', class_name: 'Event'
 
+  def self.exists?(event, attendee)
+    not find_by_event_id_and_attendee_id(event, attendee).nil?
+  end
+
+  def self.confirmed?(event, status = true, attendee)
+    find_by_event_id_and_status_and_attendee_id(event, status, attendee)
+  end
+
 end
