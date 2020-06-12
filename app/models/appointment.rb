eff82503da891b320cad18911c6023f3d2378637
jubaan <julio.ab@outlook.com>
2020-06-12 12:45:19 -0500

    Created scopes for appointment invitations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,5 @@
 class Appointment < ApplicationRecord
   belongs_to :attendee, foreign_key: 'attendee_id', class_name: 'User'
   belongs_to :event, foreign_key: 'event_id', class_name: 'Event'
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,10 @@ class Event < ApplicationRecord
   belongs_to :host, foreign_key: 'host_id', class_name: 'User'
   has_many :appointments, foreign_key: 'event_id', dependent: :destroy
   has_many :attendees, through: :appointments
+  # has_many :confirmed_appointments, foreign_key: proc{"SELECT * FROM appointments WHERE appointment.event_id = #{id} AND appointments.status = true"}
 
   scope :upcoming, -> { where('date >=?', Date.today) }
   scope :past, -> { where('date <?', Date.today) }
+  # scope :confirmed_appointments, -> { joins(:attendees).where('attendees.appointments.status = true') }
+  # scope :not_confirmed_appointments, -> { joins(:attendees).where('attendees.appointments.status = false') }
 end
