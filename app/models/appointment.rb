b4dec4e814436bae2a2dfab82c1dbed272d177ae
jubaan <julio.ab@outlook.com>
2020-06-15 17:20:28 -0500

    add relationship for invitable users, create scopes for confirmed, invited and rejected

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,12 +3,15 @@
 class Appointment < ApplicationRecord
   belongs_to :attendee, foreign_key: 'attendee_id', class_name: 'User'
   belongs_to :event, foreign_key: 'event_id', class_name: 'Event'
+  scope :accepted, -> { where ("status = 'accepted'") }
+  scope :rejected, -> { where ("status = 'rejected'") }
+  scope :invited, -> { where ("status = 'invited'") }
 
   def self.exists?(event, attendee)
     not find_by_event_id_and_attendee_id(event, attendee).nil?
   end
 
-  def self.confirmed?(event, status = true, attendee)
+  def self.confirmed?(event, status = confirmed, attendee)
     find_by_event_id_and_status_and_attendee_id(event, status, attendee)
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,12 +4,19 @@ class Event < ApplicationRecord
   belongs_to :host, foreign_key: 'host_id', class_name: 'User'
   has_many :appointments, foreign_key: 'event_id', dependent: :destroy
   has_many :attendees, through: :appointments
-  # has_many :confirmed_appointments, foreign_key: proc{"SELECT * FROM appointments WHERE appointment.event_id = #{id} AND appointments.status = true"}
+  has_many :invitable_users, through: :appointments, class_name: 'User'
+  # has_many :invited_users, through: :appointments, -> { where status: false }
+  # has_many :confirmed_users, through: :appointments, -> { where status: true }
+  # has_many :confirmed_appointments, foreign_key: proc{"SELECT * FROM appointments WHERE appointment.event_id = #{id} AND appointments.status = confirmed"}
 
   scope :upcoming, -> { where('date >=?', Date.today) }
   scope :past, -> { where('date <?', Date.today) }
 
   def self.appointments_by_status(event_id, status)
     Appointment.where("event_id = #{event_id} and status = #{status}")
+  end
+
+  def self.invitable_users(event_id)
+    self.appointments.where()
   end
 end
