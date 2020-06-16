eb2877491c81997d612c5464fe0c8d7a2747a8cd
jubaan <julio.ab@outlook.com>
2020-06-16 18:05:50 -0500

    change format to event form and event show, created has_many confirmed_users

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,16 @@ class Event < ApplicationRecord
   has_many :attendees, through: :appointments
   has_many :invitable_users, through: :appointments, class_name: 'User'
   # has_many :invited_users, through: :appointments, -> { where status: false }
-  # has_many :confirmed_users, through: :appointments, -> { where status: true }
+  has_many :confirmed_users, -> { where('status =?', 'confirmed') }, through: :appointments, class_name: 'User', source: :attendee
   # has_many :confirmed_appointments, foreign_key: proc{"SELECT * FROM appointments WHERE appointment.event_id = #{id} AND appointments.status = confirmed"}
 
   scope :upcoming, -> { where('date >=?', Date.today) }
   scope :past, -> { where('date <?', Date.today) }
 
-  def self.appointments_by_status(event_id, status)
-    Appointment.where("event_id = #{event_id} and status = #{status}")
+  # scope :confirmed_users, -> { where('attendee.appointment.status =?', 'confirmed')}
+
+  def appointments_by_status(status)
+    Appointment.where("event_id = #{self.id} and status = #{status}")
   end
 
   def invitable_users
