3055b988a84d127b1134434cc28f6d95aa6a52a2
jubaan <julio.ab@outlook.com>
2020-06-17 17:36:46 -0500

    Remove temp file code.rb

--- code.rb
+++
@@ -1,4 +0,0 @@
-
-  <% unless current_user.appointments.find_by_event_id(event.id).nil? || event.host == current_user %>
-    <%= link_to "I'm going", appointments_path({attendee_id: current_user.id, event_id: event.id, status: true}), method: :post, data: { confirm: 'Are you sure?' } %>
-  <% end %>
