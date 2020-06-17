6c87a7f39df3d650db02777d0165d6032fcc9470
fc-anjos <felipe.anjos@usp.br>
2020-06-17 12:31:10 -0300

    refactor invitable users to take into account the current_user

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,21 +4,17 @@ class Event < ApplicationRecord
   belongs_to :host, foreign_key: 'host_id', class_name: 'User'
   has_many :appointments, foreign_key: 'event_id', dependent: :destroy
   has_many :attendees, through: :appointments
-  has_many :invitable_users, through: :appointments, class_name: 'User'
-  # has_many :invited_users, through: :appointments, -> { where status: false }
   has_many :confirmed_users, -> { where('status =?', 'confirmed') }, through: :appointments, class_name: 'User', source: :attendee
-  # has_many :confirmed_appointments, foreign_key: proc{"SELECT * FROM appointments WHERE appointment.event_id = #{id} AND appointments.status = confirmed"}
+  has_many :invited_users, -> { where('status =?', 'invited') }, through: :appointments, class_name: 'User', source: :attendee
 
   scope :upcoming, -> { where('date >=?', Date.today) }
   scope :past, -> { where('date <?', Date.today) }
-
-  # scope :confirmed_users, -> { where('attendee.appointment.status =?', 'confirmed')}
 
   def appointments_by_status(status)
     Appointment.where("event_id = #{self.id} and status = #{status}")
   end
 
-  def invitable_users
-    User.all - [self.attendees, self.host]
+  def invitable_users(current_user)
+    User.all - [self.attendees, self.host, current_user]
   end
 end
