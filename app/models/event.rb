6e2bb6daf4699b3004e52a0be5102a600028e230
fc-anjos <felipe.anjos@usp.br>
2020-06-23 16:55:35 -0300

    remove I'm going button from someone else's show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   validates_uniqueness_of :title
-  validates_length_of :description, within: 50..2000
-  validates_length_of :location, within: 10..255
+  validates_length_of :description, within: 10..2000
+  validates_length_of :location, within: 4..255
   validates_presence_of :date, :title, :description, :location
 
   belongs_to :host, foreign_key: 'host_id', class_name: 'User'
