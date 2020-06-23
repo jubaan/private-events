31350c91ba1db39d79759cc2b13d858e892d88f9
fc-anjos <felipe.anjos@usp.br>
2020-06-23 16:19:13 -0300

    create past participations method for user

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :attendee, foreign_key: 'attendee_id', class_name: 'User'
   belongs_to :event, foreign_key: 'event_id', class_name: 'Event'
-  scope :past_accepted, Event.where("status = confirmed").where('date <?', Date.today).where('appointment_id =?', id)
-
-  # scope :past_accepted, -> { where "status = 'accepted' AND event.date < Date.today" }
+  scope :accepted, -> { where "status = 'accepted'" }
+  scope :rejected, -> { where "status = 'rejected'" }
+  scope :invited, -> { where "status = 'invited'" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: 'host_id', class_name: 'Event'
   has_many :appointments, foreign_key: 'attendee_id'
+
   has_many :confirmed_events,
            -> { where('status =?', 'confirmed') },
            through: :appointments,
@@ -13,10 +14,15 @@ class User < ApplicationRecord
            class_name: 'Event',
            source: :event
 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def invitable?(event_id)
     Appointment.where("attendee_id = #{id} and event_id = #{event_id}").empty?
+  end
+
+  def past_participations
+    invited_events.past + events.past
   end
 end
