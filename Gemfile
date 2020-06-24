f3c3d99a0225cdb46b6ef88c362465c69d608b95
jubaan <julio.ab@outlook.com>
2020-06-24 15:22:11 -0500

    implement Faker gem and populate db

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development do
   gem 'rails-erd', '~> 1.6'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'faker', '~> 2.13'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'date'
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,11 +7,48 @@ require 'date'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-past_date = Date.today-1
-future_date = Date.today+1
+10.times do
+  User.create(
+    {
+      name: Faker::Name.name,
+      username: Faker::Internet.username,
+      email: Faker::Internet.email,
+      password: '123456',
+      password_confirmation: '123456'
+    }
+  )
+end
 
-User.create([{name: 'julio', username:'julio', email: 'julio.ab@outlook.com',  password: 123456, password_confirmation: 123456},{name: 'miguel', username:'mighty mike', email: 'mighty@mike.com', password: 123456, password_confirmation: 123456}, {name: 'lalo', username:'lalos', email: 'lalos@lalos.com', password: 123456, password_confirmation: 123456}])
+10.times do
+  event = Event.create(
+    {
+      host_id: Faker::Number.between(from: 1, to: User.all.length),
+      title: Faker::Movie.title,
+      date: Faker::Date.between(from: '2020-01-01', to: '2021-12-31'),
+      location: Faker::TvShows::TwinPeaks.location,
+      location: Faker::TvShows::TwinPeaks.quote
+    }
+  )
 
-Event.create([{host_id: 2, title: 'new party', description: 'this is going to be the best party ever in the world so come an go wild!', location: 'the national park', date: past_date }, {host_id: 1, title: 'CRAAAAAAZY NIGHT', description: 'this is going to be the best party ever in the world so come an go wild!', location: 'the national park', date: future_date}])
+  3.times do
+    Appointment.create(
+      {
+        attendee_id: Faker::Number.between(from: 1, to: User.all.length),
+        event_id: event.id,
+        status: 'invited'
+      }
+    )
+  end
+end
 
-Appointment.create([{attendee_id: 1, event_id: 1, status: 'invited'},{attendee_id: 2, event_id: 2, status: 'invited'}])
+User.all.each do |user|
+  3.times do
+    Appointment.create(
+      {
+        event_id: Faker::Number.between(from:1, to: User.all.length),
+        attendee_id: user.id,
+        status: ['confimed', 'invited'].sample
+      }
+    )
+  end
+end
