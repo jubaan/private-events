ced98ab8afb92bdfe7595472c59351fe1cb277ac
fc-anjos <felipe.anjos@usp.br>
2020-06-24 19:51:06 -0300

    fix imports for spec

--- spec/lib/events_controller_spec.rb
+++ spec/lib/events_controller_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'rails_helper'
 
-RSpec.describe EventController < ApplicationController do
+RSpec.describe EventsController < ApplicationController do
   describe '::create' do
     it 'should create and save a new event' do
     end
