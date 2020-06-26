694acfcd38d614307cae7bcda075896e8bab8aa7
jubaan <julio.ab@outlook.com>
2020-06-25 19:11:11 -0500

    update user_spec with test methods

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -2,6 +2,16 @@ FactoryBot.define do
   factory :appointment, class: Appointment do
     # attendee_id { Faker::Number.between(from: 1, to: User.all.length) }
     # event_id { Faker::Number.between(from: 1, to: User.all.length) }
-    status { ['confirmed', 'invited'].sample }
+    trait :status_random do
+      status { ['confirmed', 'invited'].sample }
+    end
+
+    trait :status_invited do
+      status { 'invited' }
+    end
+
+    trait :status_confirmed do
+      status { 'confirmed' }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,31 @@ RSpec.describe User, type: :model do
         expect(user.events).to include(event)
       end
     end
+  end
 
+  describe '#host?' do
+    let (:user) { create :user, :with_username }
+    let (:event) { create(:event, host_id: user.id) }
+    it 'should return true if the user is the host of the event' do
+      expect(user.host?(event.id)).to be_truthy
+    end
+  end
+
+  describe '#current_user?' do
+    let (:current_user) { create :user, :with_username }
+    it 'should return true if the user is the host of the event' do
+      expect(current_user.current_user?(current_user)).to be_truthy
+    end
+  end
+
+  describe '#invited?' do
+    let (:attendee) { create :user, :with_username }
+    let (:host) { create :user, :with_username }
+    let (:event) { create(:event, host_id: host.id) }
+    let (:appointment) { create(:appointment, :status_invited, attendee_id: attendee.id, event_id: event.id) }
+    it 'should return true if the user is invited to the event' do
+      # byebug
+      expect(appointments.attendee && attendee.invited?(event.id)).to be_truthy
+    end
   end
 end
